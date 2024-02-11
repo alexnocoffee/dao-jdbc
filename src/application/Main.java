7693bf7092687d156bd8e40d53d9340fd5831058
alexnocoffee <contato.nocoffee@gmail.com>
2024-02-11 20:31:16 -0300

    findById implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,28 +2,16 @@ package application;
 
 import dao.DaoFactory;
 import dao.SellerDao;
-import entities.Department;
 import entities.Seller;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
 
-        Department department = new Department(2, "Books");
-
-        Seller seller = new Seller(22,
-                "Alex",
-                "alex@gmail.com",
-                LocalDate.parse("25/08/2006", DateTimeFormatter.ofPattern("dd/MM/yyyy")),
-                3000.00,
-                department);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        Seller seller = sellerDao.findById(3);
+
         System.out.println(seller);
-        System.out.println(sellerDao);
 
     }
 }
