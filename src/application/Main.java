0e787ade230c0c7b3c39c6a4851ce1eb8ce65c83
alexnocoffee <contato.nocoffee@gmail.com>
2024-02-11 18:17:32 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,24 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
 
         Department department = new Department(2, "Books");
-        System.out.println(department);
+
+        Seller seller = new Seller(22,
+                "Alex",
+                "alex@gmail.com",
+                LocalDate.parse("25/08/2006", DateTimeFormatter.ofPattern("dd/MM/yyyy")),
+                3000.00,
+                department);
+
+        System.out.println(seller);
 
     }
 }
